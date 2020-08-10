ed623edbdd00ccf38f6d8c51511c9cebabfcc783
andryushque <andrew.ustinov23@gmail.com>
2020-08-10 23:13:15 +0300

    Create: install section

--- js/main.js
+++ js/main.js
@@ -18,4 +18,15 @@ $(document).ready(function () {
 
   /*=== Mobile Menu Accordeon ===*/
   new AccordionMenu(".mmenu-accordion");
+
+  /*=== Install Tabs ===*/
+  var tabsItem = $(".tabs__item");
+  var contentItem = $(".content__item");
+  tabsItem.on("click", function () {
+    var activeContent = $(this).attr("data-target");
+    contentItem.removeClass("content__item--active");
+    tabsItem.removeClass("tabs__item--active");
+    $(activeContent).addClass("content__item--active");
+    $(this).addClass("tabs__item--active");
+  });
 });
